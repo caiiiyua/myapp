6938daaf7fc67da4a4b87d7759fe66eca456fc65
Yuanqing Cai <ckjacket@gmail.com>
2015-04-23 20:19:19 +0800

    test pospal api

--- public/js/app.js
+++ public/js/app.js
@@ -10,3 +10,38 @@ function redirectTo(link, delay) {
 		location.href = link
 	}, delay)
 }
+
+function SendOrder() {
+	var name = "刘丽萍"
+	var phone = "18615774413"
+	$.ajax({
+	    type: "POST",
+	    async: false,
+	    cache: false,
+	    dataType: 'json',
+	    contentType: 'application/json; charset=utf-8',
+	    url: "naiping.pospal.cn/order/place",
+	    data: JSON.stringify({
+	        customerName: name,
+	        customerPhoneNum: phone,
+	        customerAddress: "test",
+	        orderComment: "",
+	        paymentMethod: "CustomerBalance"
+	    }),
+	    error: function (XmlHttpRequest, textStatus, errorThrown) { alert("下单出现错误！"); },
+	    success: function (data) {
+	        if (data.status) {
+	            if (data.isHtml) {
+	                window.open(data.redirectUrl);
+	                $("body").append(data.html);
+	            }
+	            else {
+	                window.location.replace(data.redirectUrl);
+	            }
+	        }
+	        else {
+	            alert(data.message);
+	        }
+		}
+	});
+}
